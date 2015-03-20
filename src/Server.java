48b7b4be878b079d5f385bcb5dfc0a7d3c9d5771
tseiwert19@googlemail.com <ts190390>
2015-03-20 15:12:13 +0100

    server db eingefügt
    server klasse konvertiert

--- src/Server.java
+++ src/Server.java
@@ -5,4 +5,62 @@
  */
 public class Server {
 
+	/**
+	 * Der Server sendet eine Anfrage zu allen anderen Servern.
+	 */
+	private void sendRequestToAllOtherServers() {
+		// zuerst einmal nicht wichtig
+	}
+
+	/**
+	 * Der Server fordert ein Video von einem anderen Server an.
+	 */
+	private void requestVideo() {
+		// zuerst einmal nicht wichtig
+	}
+
+	/**
+	 * Der Server sendet ein Video zu dem anfragenden Client.
+	 */
+	private void sendVideo(String videoName) {
+		// sehr wichtig
+	}
+
+	/**
+	 * Der Server stellt seine Datenbank, mit Hilfe der anderen Servern, wieder
+	 * her.
+	 */
+	private void restoreDatabase() {
+		// zuerst einmal nicht wichtig
+	}
+
+	/**
+	 * fügt ein neues Video eines anderen Servers oder Clients ein
+	 */
+	private void insertNewVideo(String videoName) {
+		// sehr wichtig
+	}
+
+	/**
+	 * fügt eine, vom Client vorgeschlagene, Übersetzung ein
+	 */
+	private void insertNewTranslation(String VideoName, String neueBezeichnung) {
+		// sehr wichtig
+	}
+
+	/**
+	 * reserviert eine ID für ein neues Video und informiert alle anderen
+	 * Server, dass diese ID belegt ist
+	 */
+	private void reserveID() {
+// muss abgeklärt werden
+	}
+
+	/**
+	 * vergleicht die ID eines Videos mit der in der eigenen Datenbank
+	 */
+	private void compareID(int id) {
+// abklären
+	}
+
 }
