035fe89eee561ba113c7b57492812d09a81dd416
michael <michael@michael-System-Product-Name>
2015-03-21 19:22:08 +0100

    Parser für Videos

---
+++ src/VideoParser.java
@@ -0,0 +1,110 @@
+
+
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Mappt einen Datensatz aus der Datenbank auf ein Video Objekt.
+ * @author michael
+ *
+ */
+public class VideoParser
+{
+    private DatenbankController dbController;
+
+    public VideoParser()
+    {
+        dbController = new DatenbankController();
+    }
+
+    /**
+     * Mappt alle Datensaetze aus der Datenbank, die mit name uebereinstimmen, auf Video-Objekte
+     * @param name
+     * @return Liste mit Videos
+     */
+    public ArrayList<Video> mappeVideosVonName(String name)
+    {
+        ResultSet ergebnis = dbController.getAllByName(name);
+        return parseVideos(ergebnis);
+    }
+    /**
+     * Mappt alle Datensaetze aus der Datenbank, die mit i uebereinstimmen, auf Video-Objekte
+     * @param i = Ampelfarbe (0=grün, 1=gelb, 2=rot)
+     * @return Liste mit Videos
+     */
+    public ArrayList<Video> mappeVideosVonAmpel(int i, String sprache){
+    	ResultSet ergebnis = dbController.getAllByAmpel(i, sprache);
+    	return parseVideos(ergebnis);
+    }
+   
+    /**
+     * Mappt einen Datensatz auf ein Video-Objekt
+     * @param id
+     * @return Video
+     */
+    public Video mappeEinVideo(int id, String sprache)
+    {
+        Video video;
+        ResultSet ergebnis = dbController.getEntry(id, sprache);
+        ArrayList<Video> videoListe = parseVideos(ergebnis);
+        if (videoListe.size() == 1)
+        {
+            video = videoListe.get(0);
+        }
+        else
+        {
+            video = null;
+        }
+
+        return video;
+    }
+    /**
+     * Uebernimmt das Parsen eines ResultSets
+     * @param ergebnis
+     * @return Liste mit Videos
+     */
+    private ArrayList<Video> parseVideos(ResultSet ergebnis)
+    {
+        int id;
+        String name;
+        String pfad;
+        String beschreibung;
+        String geraet;
+        String schwierigkeitsgrad;
+        String elementgruppe;
+        int ampel;
+        Video video;
+        ArrayList<Video> videos = new ArrayList<Video>();
+
+
+        try
+        {
+            while (ergebnis.next())
+            {
+                id = ergebnis.getInt("id");
+                name = ergebnis.getString("videoname");
+                geraet = "";
+                pfad = "";
+                beschreibung = "";
+                schwierigkeitsgrad = "";
+                elementgruppe = "";
+                ampel = ergebnis.getInt("ampel");
+
+                video = new Video(id, name, pfad, geraet, beschreibung, schwierigkeitsgrad,
+                        elementgruppe, ampel);
+                videos.add(video);
+
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Fehler bei Datenbankabfrage!");
+            e.printStackTrace();
+        }
+        return videos;
+    }
+   
+}
