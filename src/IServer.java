b2a392195bf176a1359ec7efa6f8e3fa2d0eaac1
michael <michael@michael-System-Product-Name>
2015-03-21 19:20:47 +0100

    findVideo() insertNewTranslation() insertNewVideo() hinzugefügt

--- src/IServer.java
+++ src/IServer.java
@@ -1,6 +1,10 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface  IServer extends Remote {
 	public String insert(String neueBezeichnung) throws RemoteException ;
+	public List<Video> findVideo(String name)throws RemoteException;
+	public void insertNewTranslation(int id, String neueBezeichnung, String sprache) throws RemoteException;
+	public void insertNewVideo(int id, String name, int ampel, String sprache) throws RemoteException;
 }
