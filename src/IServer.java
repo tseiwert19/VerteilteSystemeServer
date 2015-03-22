2156333d60013d2f6b1879e4068ea27a68bc33cd
michael <michael@michael-System-Product-Name>
2015-03-22 17:44:53 +0100

    Neue Funktionen eingefügt

--- src/IServer.java
+++ src/IServer.java
@@ -8,4 +8,9 @@ public interface  IServer extends Remote {
 	public List<Video> findVideo(String name)throws RemoteException;
 	public void insertNewTranslation(int id, String neueBezeichnung, String sprache) throws RemoteException;
 	public void insertNewVideo(int id, String name, int ampel, String geraet, String beschreibung,String schwierigkeitsgrad, String elementgruppe, File video,  String sprache) throws RemoteException;
+	public void reserveId(int id) throws RemoteException;
+	public String getServerLanguage() throws RemoteException;
+	public List<Video> getAllVideosByLanguage(String language) throws RemoteException;
+	public void restoreDatabase() throws RemoteException;
+
 }
