520691cf184783eb216f3a8f73e9518e1cb25a67
tseiwert19@googlemail.com <ts190390>
2015-03-22 15:10:04 +0100

    unwichtig

---
+++ src/Serverunwichtig.java
@@ -0,0 +1,42 @@
+
+public class Serverunwichtig {
+	/*
+	 * ---------------------------------unwichtig ----------------------
+	 */
+	/**
+	 * Der Server fordert ein Video von einem anderen Server an.
+	 */
+	private void requestVideo() {
+		// zuerst einmal nicht wichtig
+	}
+
+	/**
+	 * Der Server stellt seine Datenbank, mit Hilfe der anderen Servern, wieder
+	 * her.
+	 */
+	private void restoreDatabase() {
+		// zuerst einmal nicht wichtig
+	}
+
+	/**
+	 * reserviert eine ID für ein neues Video und informiert alle anderen
+	 * Server, dass diese ID belegt ist
+	 */
+	private void reserveID() {
+		// muss abgeklärt werden
+	}
+
+	/**
+	 * vergleicht die ID eines Videos mit der in der eigenen Datenbank
+	 */
+	private void compareID(int id) {
+		// abklären
+	}
+
+	/**
+	 * Der Server sendet eine Anfrage zu allen anderen Servern.
+	 */
+	private void sendRequestToAllOtherServers() {
+		// zuerst einmal nicht wichtig
+	}
+}
