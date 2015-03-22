b4003dbd8947c7b79656d16d04f610b6b183401a
michael <michael@michael-System-Product-Name>
2015-03-22 17:46:07 +0100

    Viele neue Funktionen implementiert (noch nicht getestet!)

--- src/KampfrichterServer.java
+++ src/KampfrichterServer.java
@@ -1,10 +1,13 @@
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +16,23 @@ import java.util.List;
  * @author Thomas
  */
 public class KampfrichterServer extends UnicastRemoteObject implements IServer {
+	/**
+	 * Adressen zu den Servern
+	 */
+	public static final String SERVER_DEUTSCHLAND = "";
+	public static final String SERVER_FRANKREICH = "";
+	public static final String SERVER_ENGLAND = "";
+
+	public static final String SERVER_LANGUAGE = DatenbankController.GERMAN;
+	private static final String RESERVED = "RESERVED";
+	/**
+	 * Liste mit allen Server-Adressen
+	 */
+	private List<URL> server;
+	/**
+	 * Liste mit allen Sprachen
+	 */
+	private List<String> languages;
 
 	public static void main(String[] args) {
 		try {
@@ -31,8 +51,17 @@ public class KampfrichterServer extends UnicastRemoteObject implements IServer {
 		}
 	}
 
-	protected KampfrichterServer() throws RemoteException {
+	protected KampfrichterServer() throws RemoteException,
+			MalformedURLException {
 		super();
+		// server = new ArrayList<URL>();
+		// server.add(new URL(SERVER_DEUTSCHLAND));
+		// server.add(new URL(SERVER_FRANKREICH));
+		// server.add(new URL(SERVER_ENGLAND));
+		// languages = new ArrayList<String>();
+		// languages.add(DatenbankController.GERMAN);
+		// languages.add(DatenbankController.ENGLISH);
+		// languages.add(DatenbankController.FRENCH);
 	}
 
 	private static final long serialVersionUID = 3701445934486704839L;
@@ -50,9 +79,24 @@ public class KampfrichterServer extends UnicastRemoteObject implements IServer {
 	/**
 	 * f�gt eine, vom Client vorgeschlagene, �bersetzung ein
 	 */
-	public void insertNewTranslation(int id, String neueBezeichnung, String sprache) throws RemoteException {
+	public void insertNewTranslation(int id, String neueBezeichnung,
+			String sprache) throws RemoteException {
 		DatenbankController dbController = new DatenbankController();
 		dbController.updateTranslation(id, neueBezeichnung, sprache);
+		//updateTranslationOnOtherServers(id, neueBezeichnung, SERVER_LANGUAGE)
+	}
+	
+	private void updateTranslationOnOtherServers(int id, String neueBezeichnung, String sprache){
+		for (URL tmp : server) {
+			try {
+				IServer iserver = (IServer) Naming.lookup(tmp.toString());
+				iserver.insertNewTranslation(id, neueBezeichnung, sprache);
+			} catch (MalformedURLException | RemoteException
+					| NotBoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
@@ -60,16 +104,141 @@ public class KampfrichterServer extends UnicastRemoteObject implements IServer {
 	 */
 	public List<Video> findVideo(String name) throws RemoteException {
 		VideoParser parser = new VideoParser();
-		return parser.mappeVideosVonName(name);
-
+		List<Video> videos = parser.mappeVideosVonName(name);
+		//TODO testen
+		//if (videos.size() == 0) {
+		//	videos = findVideoOnOtherServers(name);
+		//}
+		return videos;
 	}
 
 	/**
 	 * f�gt ein neues Video eines anderen Servers oder Clients ein
 	 */
-	public void insertNewVideo(int id, String name, int ampel, String geraet, String beschreibung,String schwierigkeitsgrad,  String elementgruppe, File video,  String sprache) throws RemoteException{
+	public void insertNewVideo(int id, String name, int ampel, String geraet,
+			String beschreibung, String schwierigkeitsgrad,
+			String elementgruppe, File video, String sprache)
+			throws RemoteException {
 		DatenbankController dbController = new DatenbankController();
-		dbController.addVideo(id, name, ampel, geraet, beschreibung,schwierigkeitsgrad, elementgruppe, video, sprache);
+		dbController.addVideo(id, name, ampel, geraet, beschreibung,
+				schwierigkeitsgrad, elementgruppe, video, sprache);
+		// Nach einem Insert sollten andere Server die ID reservieren
+		//TODO testen
+	//	if(sprache.equals(SERVER_LANGUAGE)){
+	//		informServerAboutReservation(id);
+	//	}
+		
+	}
+
+	/**
+	 * Server reserviert ID in der Datenbank
+	 */
+	public void reserveId(int id) throws RemoteException {
+		DatenbankController dbController = new DatenbankController();
+		dbController.addVideo(id, RESERVED, DatenbankController.RED, "", "",
+				"", "", null, SERVER_LANGUAGE);
+
+	}
+
+	/**
+	 * Informiert alle anderen Server darüber, dass eine ID reserviert werden
+	 * muss
+	 * 
+	 * @param id
+	 *            zu reservierende ID
+	 */
+	private void informServerAboutReservation(int id) {
+		for (URL tmp : server) {
+			try {
+				IServer iserver = (IServer) Naming.lookup(tmp.toString());
+				iserver.reserveId(id);
+			} catch (MalformedURLException | RemoteException
+					| NotBoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Der Server sucht Video auf anderen Servern
+	 */
+	private List<Video> findVideoOnOtherServers(String name) {
+		List<Video> videos = new ArrayList<Video>();
+		for (URL tmp : server) {
+			try {
+				IServer iserver = (IServer) Naming.lookup(tmp.toString());
+				videos = iserver.findVideo(name);
+				if (videos.size() != 0) {
+					String sprache = iserver.getServerLanguage();
+					//Fügt Videos in eigene Datenbank
+					for (Video video : videos) {
+						insertNewVideo(video.getId(), video.getName(), video.getAmpel(),
+								video.getGeraet(), video.getBeschreibung(),
+								video.getSchwierigkeitsgrad(), video.getElementgruppe(),
+								null, sprache);
+					}
+					return videos;
+				}
+			} catch (MalformedURLException | RemoteException
+					| NotBoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return videos;
+	}
+	
+	public String getServerLanguage() throws RemoteException{
+		return SERVER_LANGUAGE;
+	}
+	/**
+	 * Holt alle Videos einer Sprache von den anderen Servern
+	 */
+	private List<Video> restoreVideos() throws RemoteException{
+		List<Video> alleVideos = new ArrayList<Video>();
+		List<Video> videos;
+		for (URL tmp : server) {
+			try {
+				IServer iserver = (IServer) Naming.lookup(tmp.toString());
+				videos = iserver.getAllVideosByLanguage(SERVER_LANGUAGE);
+				for(Video video : videos){
+					if(!alleVideos.contains(video)){
+						alleVideos.add(video);
+					}
+				}
+				
+			} catch (MalformedURLException | RemoteException
+					| NotBoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return alleVideos;
+	}
+	/**
+	 * Gibt alle Videos einer Sprache zurück
+	 */
+	public List<Video> getAllVideosByLanguage(String language) throws RemoteException{
+		VideoParser parser = new VideoParser();
+		return parser.mappeVideosVonSprache(language);
+	}
+	
+	/**
+	 * Der Server stellt seine Datenbank, mit Hilfe der anderen Servern, wieder
+	 * her.
+	 * @throws RemoteException 
+	 */
+	public void restoreDatabase() throws RemoteException {
+		List<Video> videos = restoreVideos();
+		DatenbankController dbController = new DatenbankController();
+		dbController.createDatabase();
+		for(Video video : videos){
+			dbController.addVideo(video.getId(), video.getName(), video.getAmpel(),
+			video.getGeraet(), video.getBeschreibung(),
+			video.getSchwierigkeitsgrad(), video.getElementgruppe(),
+			null, SERVER_LANGUAGE);
+		}
 	}
 
 }
